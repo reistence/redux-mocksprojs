a2b3b44b2eab73ad84b13c4008c4d9da4364e2de
reistence <gvlamberti@gmail.com>
2023-05-18 12:39:50 +0200

    post timestamp added and sorting'em by it

--- src/features/posts/postsSlice.jsx
+++ src/features/posts/postsSlice.jsx
@@ -1,33 +1,40 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
+import sub from "date-fns/sub";
 
 const initialState = [
   {
     id: 1,
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
     title: "Redux 1st projects",
     content: "Been implementing a simple counter as warm up",
   },
   {
     id: 2,
+    date: sub(new Date(), { minutes: 15 }).toISOString(),
     title: "Redux 2nd projects",
     content: "Been implementing a simple blog as second project",
   },
   {
     id: 3,
+    date: sub(new Date(), { minutes: 17 }).toISOString(),
     title: "Redux 2nd projects",
-    content: "Been implementing a simple blog as second project",
+    content: "Been implementing a simple blg as second pro)ject",
   },
   {
     id: 4,
+    date: sub(new Date(), { minutes: 20 }).toISOString(),
     title: "Redux 2nd projects",
     content: "Been implementing a simple blog as second project",
   },
   {
     id: 5,
+    date: sub(new Date(), { minutes: 21 }).toISOString(),
     title: "Redux 2nd projects",
     content: "Been implementing a simple blog as second project",
   },
   {
     id: 6,
+    date: sub(new Date(), { minutes: 23 }).toISOString(),
     title: "Redux 2nd projects",
     content: "Been implementing a simple blog as second project",
   },
@@ -48,6 +55,7 @@ const postsSlice = createSlice({
             title,
             content,
             userId,
+            date: new Date().toISOString(),
           },
         };
       },
